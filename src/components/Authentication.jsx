f90e1af2b666586a8dd19f1bc05b65ad38d96d07
Georgiy <hantertomson@gmail.com>
2023-08-30 19:12:45 +0200

    something_unknown_done

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-const Authentication = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+export default function Authentication() {
+  const [err, setErr] = useState(null);
+  const handleClose = () => setErr(null);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    try {
+      const res = await axios.post( '/api/auth/login',data);
+      if (res.status === 200) window.location = '/';
+    } catch (error) { setErr(error.response.data.massage); 
+    }
+  };   
+  return ( 
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
+<Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
-  return (
-    <div>
-      {isLoggedIn ? (
-        <button onClick={handleLogout}>Logout</button>
-      ) : (
-        <button onClick={handleLogin}>Login</button>
-      )}
-    </div>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
-};
-
-export default Authentication;
+}
