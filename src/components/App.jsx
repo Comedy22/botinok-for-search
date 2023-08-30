9eefe5c837ad74229819527cb024d757062a1345
Comedy22 <zaken22@bk.ru>
2023-08-30 18:53:00 +0300

    nav bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import NavBar from './NavBar';
 
-export default function App({ hello }) {
-  return <div>Hello, {hello}</div>;
+export default function App() {
+  return (
+    <NavBar />
+  );
 }
