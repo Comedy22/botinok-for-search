68b0cda6d233cb7e0a9020cabae51768a69e0c85
Comedy22 <zaken22@bk.ru>
2023-08-31 07:18:21 +0300

    release 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 
 export default function App() {
   return (
     <NavBar />
-  );
+  )
 }
