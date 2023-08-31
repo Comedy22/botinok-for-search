ccf3fa92147260cf2294dbd893d7c129b72c96b1
Vazgen <Kirakosyan_Vazgen@mail.ru>
2023-08-31 15:58:27 +0300

    logout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
         <Nav.Link href="/">Home</Nav.Link>
       </Nav.Item>
       <Nav.Item as="li">
-        <Nav.Link href="/api/signup">Sign up!</Nav.Link>
+        <Nav.Link href="/signup">Sign up!</Nav.Link>
       </Nav.Item>
       <Nav.Item as="li">
         <Nav.Link href="/login">Sign in!</Nav.Link>
